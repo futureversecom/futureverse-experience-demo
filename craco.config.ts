72f60be83ad10b5257d484f97b9c647413c23140
kabece <13706738+kabece@users.noreply.github.com>
2023-09-20 15:50:18 +1200

    Disable Source Map generation

--- craco.config.ts
+++ craco.config.ts
@@ -1,6 +1,10 @@
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin')
+
 const config = {
   webpack: {
     configure: {
+      plugins: [new NodePolyfillPlugin()],
+
       module: {
         rules: [
           {
